2c18fdecc3a976e56f1a02a44bcb5423a4d71c39
Matthew Ramos <mmramos309@gmail.com>
2019-12-15 19:42:17 -0500

    Created cocoa touch file for custom cell, aligned custom cell features

---
+++ Pursuit-Core-iOS-Episodes-from-Online/Pursuit-Core-iOS-Episodes-from-Online/ShowCell.swift
@@ -0,0 +1,14 @@
+//
+//  ShowCell.swift
+//  Pursuit-Core-iOS-Episodes-from-Online
+//
+//  Created by Matthew Ramos on 12/15/19.
+//  Copyright © 2019 Benjamin Stone. All rights reserved.
+//
+
+import UIKit
+
+class ShowCell: UITableViewCell {
+
+  
+}
